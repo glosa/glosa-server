a9ab1ef6c97b42f611dd80e852665a7a40eb964b
Andros Fenollosa <andros@fenollosa.email>
2020-03-07 17:36:08 +0100

    Update view

--- src/glosa/views/public.clj
+++ src/glosa/views/public.clj
@@ -2,7 +2,17 @@
 (ns glosa.views.public
   (:require
    [tadam.templates :refer [render-JSON]]
+   [cheshire.core :refer [generate-stream]]
+   [clojure.java.io :as io]
    ))
+
+(def db {})
+(def db-path "db.json")
+
+(defn db-save
+  "Save database"
+  [update-data]
+  (generate-stream update-data (clojure.java.io/writer (.getFile (io/resource db-path)))))
 
 (defn get-comments
   ;; View JSON
@@ -10,6 +20,6 @@
   (render-JSON req {:url (-> req :params :url)}))
 
 (defn status-404
-;; View page 404
-[req]
-(render-JSON req {:status 404}))
+  ;; View page 404
+  [req]
+  (render-JSON req {:status 404}))
