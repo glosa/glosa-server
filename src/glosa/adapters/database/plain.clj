6cb41dab680a44d10b2c896995c5f78053fbef15
Andros Fenollosa <andros@fenollosa.email>
2020-04-10 19:20:58 +0200

    Add deep

--- src/glosa/adapters/database/plain.clj
+++ src/glosa/adapters/database/plain.clj
@@ -23,22 +23,32 @@
 
 (db-load)
 
+(defn get-deep
+  "Calculate the depth of the commentary. If it is a parent, it will return 0, if it is a sub-comment 1, if it is a sub-comment of a sub-comment 2..."
+  [id parents]
+  (let [comment      (first (filter (fn [comment] (= (:id comment) id)) @db))
+        parent-id    (:parent comment)
+        parents-temp (if (or (empty? parents) (nil? parents)) [] parents)]
+    (if (empty? (str parent-id))
+      (count parents-temp)
+      (recur parent-id (if (empty? (str parent-id)) [] (conj parents parent-id))))))
+
 (defn get-comments
-  "Find comments from url"
+  "Find comments from url. Sort by createdAt and Add deep value"
   [url]
-  (sort-by :createdAt (filter #(= (:thread %) url) @db)))
+  (map (fn [comment] (assoc comment :deep (get-deep (:id comment) nil))) (sort-by :createdAt (filter #(= (:thread %) url) @db))))
 
 (defn get-new-id
-  "Generate a new id by finding out which is the highest id and uploading one"
-  []
-  (inc (reduce (fn [id item] (if (< id (:id item)) (:id item) id)) 0 @db)))
+"Generate a new id by finding out which is the highest id and uploading one"
+[]
+(inc (reduce (fn [id item] (if (< id (:id item)) (:id item) id)) 0 @db)))
 
 (defn add-comment
-  "Add new comment"
-  [parent author message token thread]
-  (if (captcha/check-token token thread)
-    (let [update-db (conj @db {:id (get-new-id) :parent parent :createdAt (utils/get-unixtime-now) :thread thread :author author :message message})]
-      (reset! db update-db)
-      (db-save @db)
-      true)
-    false))
+"Add new comment"
+[parent author message token thread]
+(if (captcha/check-token token thread)
+  (let [update-db (conj @db {:id (get-new-id) :parent parent :createdAt (utils/get-unixtime-now) :thread thread :author author :message message})]
+    (reset! db update-db)
+    (db-save @db)
+    true)
+  false))
