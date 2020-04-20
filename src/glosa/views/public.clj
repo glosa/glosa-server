114c48476d6cff3c7136b51a7b960b036f0ebe4f
Andros Fenollosa <andros@fenollosa.email>
2020-04-20 13:19:09 +0200

    Remove valid domain function

--- src/glosa/views/public.clj
+++ src/glosa/views/public.clj
@@ -3,37 +3,30 @@
   (:require
    [tadam.templates :refer [render-JSON]]
    [tadam.utils :refer [get-JSON]]
-   [glosa.config :refer [config]]
    [glosa.ports.database :as database]
    [glosa.ports.notify :as notify]
    [glosa.ports.captcha :as captcha]))
 
-(defn is-valid-domain
-  "Check if the request comes from a valid domain"
-  [req]
-  (= (get-in req [:headers "referer"]) (config :domain-cli)))
-
 (defn get-comments
   "All comments from url"
   [req]
-  (render-JSON req (if (is-valid-domain req) (database/get-comments (-> req :params :url)) {:status 401})))
+  (render-JSON req (database/get-comments (-> req :params :url))))
 
 (defn add-comment
   "Add new comment"
   [req]
-  (if (is-valid-domain req)
-    (let [my-json (get-JSON req)]
-      (if (database/add-comment (:parent my-json) (:author my-json) (:message my-json) (:token my-json) (:thread my-json))
-        (do
-          (notify/send (format "Author: %s\nMessage: %s\nThread: %s" (:author my-json) (:message my-json) (:thread my-json)))
-          (render-JSON req {} 200))
-        (render-JSON req {} 401)))
-    (render-JSON req {} 401)))
+  (let [my-json (get-JSON req)]
+    (if (database/add-comment (:parent my-json) (:author my-json) (:message my-json) (:token my-json) (:thread my-json))
+      (do
+        (notify/send (format "Author: %s\nMessage: %s\nThread: %s" (:author my-json) (:message my-json) (:thread my-json)))
+        (render-JSON req {} 200))
+      (render-JSON req {} 401)))
+  (render-JSON req {} 401))
 
 (defn get-captcha
   "Get token captcha"
   [req]
-  (render-JSON req (if (is-valid-domain req) (assoc {} :token (captcha/get-token (-> req :params :url))) {:status 401})))
+  (render-JSON req (assoc {} :token (captcha/get-token (-> req :params :url)))))
 
 (defn status-404
   "Page 404"
