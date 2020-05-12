bcd7ebf09315eaa5e15d44b92cef11d2e5ab83fb
Andros Fenollosa <andros@fenollosa.email>
2020-05-12 23:08:18 +0200

    Update version 1.2.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject glosa "1.1.0"
+(defproject glosa "1.2.0"
   :description "glosa"
   :url "https://github.com/tanrax/glosa"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
