09a70fe6ad0c5847c09511d9e5638e5fdb28973b
Andros Fenollosa <andros@fenollosa.email>
2020-10-11 01:16:22 +0200

    Add checkpoint

--- src/glosa/views/private.clj
+++ src/glosa/views/private.clj
@@ -6,22 +6,21 @@
    [tadam.templates :refer [render-JSON]]
    [tadam.responses :refer [response]]
    [tadam.utils :refer [get-JSON]]
-   [glosa.ports.database :as database]
-   [glosa.ports.notify :as notify]
-   [glosa.ports.captcha :as captcha]))
+   [glosa.ports.database :as database]))
 
 (defn check-bearer-token
   [req]
-  (let [authorization-raw    (or (:Authorization (get-JSON req)) "")
+  (let [authorization-raw    (or (-> req :headers (get "authorization")) "")
         authorization-bearer (s/trim (s/replace authorization-raw #"^Bearer " ""))
-        admin-token          (:admin-token config)]
-    (= authorization-bearer admin-token)))
+        token                (:token config)]
+    (= authorization-bearer token)))
 
 (defn response-401
   [req]
-  (response (assoc req :WWW-Authenticate "Basic realm=\"Wrong administrator token\"") "" 401 ""))
+  (response (assoc req :WWW-Authenticate "Basic realm=\"Wrong token\"") "" 401 ""))
 
 (defn get-threads
   "Search threads"
   [req]
+  (prn (check-bearer-token req))
   (if (check-bearer-token req) (render-JSON req (database/get-threads (or (:search (get-JSON req)) ""))) (response-401 req)))
